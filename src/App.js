2c0ba31c56fc7aa078d6d2ea063d2e7c3ce2edd5
Gladstone Rose <gladstonerose@hotmail.com>
2023-06-23 00:24:20 -0500

    initial addition of files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+/*
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+Gladdy success
+    </div>
+  );
+}
+
+export default App;
+
+*/
+
+
+
+import React from 'react';
+import Profile from './Profile'
+
+function App() {
+    return <Profile />
+}
+
+export default App;
